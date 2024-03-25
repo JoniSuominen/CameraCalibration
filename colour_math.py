e17208b2f5468fecc04401e4f6c3fb204d3d0868
Joni Suominen <joni.suominen98@gmail.com>
2024-03-25 18:38:30 +0200

    revert back from colour training data generation due to incompability with sigma wb

--- colour_math.py
+++ colour_math.py
@@ -4,31 +4,141 @@ from colour.plotting import plot_chromaticity_diagram_CIE1931
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 import colour 
+import torch
 
-def compute_response(reflectance,  illuminant, sensitivities, wb=False):
-    response = reflectance.values.T @ np.diag(illuminant.values) @ sensitivities.values
-    max_response = compute_response_perfect_reflector(sensitivities, illuminant)
-    if wb:
-        response /= max_response
-    else:
-        response /= max_response[1]
-    return response
+def degrees(n): return n * (180. / np.pi)
+def radians(n): return n * (np.pi / 180.)
+def hpf_diff(x, y):
+    mask1=((x == 0) * (y == 0)).float()
+    mask1_no = 1-mask1
+
+    tmphp = degrees(torch.atan2(x*mask1_no, y*mask1_no))
+    tmphp1 = tmphp * (tmphp >= 0).float()
+    tmphp2 = (360+tmphp)* (tmphp < 0).float()
+
+    return tmphp1+tmphp2
+
+def dhpf_diff(c1, c2, h1p, h2p):
+
+    mask1  = ((c1 * c2) == 0).float()
+    mask1_no  = 1-mask1
+    res1=(h2p - h1p)*mask1_no*(torch.abs(h2p - h1p) <= 180).float()
+    res2 = ((h2p - h1p)- 360) * ((h2p - h1p) > 180).float()*mask1_no
+    res3 = ((h2p - h1p)+360) * ((h2p - h1p) < -180).float()*mask1_no
+
+    return res1+res2+res3
+
+def ahpf_diff(c1, c2, h1p, h2p):
+
+    mask1=((c1 * c2) == 0).float()
+    mask1_no=1-mask1
+    mask2=(torch.abs(h2p - h1p) <= 180).float()
+    mask2_no=1-mask2
+    mask3=(torch.abs(h2p + h1p) < 360).float()
+    mask3_no=1-mask3
+
+    res1 = (h1p + h2p) *mask1_no * mask2
+    res2 = (h1p + h2p + 360.) * mask1_no * mask2_no * mask3 
+    res3 = (h1p + h2p - 360.) * mask1_no * mask2_no * mask3_no
+    res = (res1+res2+res3)+(res1+res2+res3)*mask1
+    return res*0.5
+
+def xyz_to_lab(xyz):
+    # Define constants for the transformation
+    epsilon = 0.008856
+    kappa = 903.3
+    X_n = 0.95047
+    Y_n = 1.00000
+    Z_n = 1.08883
+
+    # Normalizing by the reference white point
+    xyz_scaled = xyz / torch.tensor([X_n, Y_n, Z_n])
+
+    # Calculate f(t) for XYZ
+    f_xyz = torch.where(xyz_scaled > epsilon, xyz_scaled.pow(1/3), (kappa * xyz_scaled + 16) / 116)
+
+    # Calculate Lab components
+    L = 116 * f_xyz[:, 1] - 16
+    a = 500 * (f_xyz[:, 0] - f_xyz[:, 1])
+    b = 200 * (f_xyz[:, 1] - f_xyz[:, 2])
+
+    # Combine L, a, b
+    lab = torch.stack([L, a, b], dim=1)
+
+    return lab
 
 
-def compute_response_perfect_reflector(sensitivities, illuminant):
-    response_test = np.diag(illuminant.values) @ sensitivities.values
-    response_sum = np.sum(response_test, axis=0)
+def ciede2000_diff(xyz1, xyz2):
+    '''
+    CIEDE2000 metric to claculate the color distance map for a batch of image tensors defined in CIELAB space
     
-    return response_sum
-def wb(response_macbeth, responses):
+    '''
+    Lab1 = xyz_to_lab(xyz1)
+    Lab2 = xyz_to_lab(xyz2)
     
-    r_Gain = response_macbeth[18, 0] / response_macbeth[18, 1]
-    b_Gain = response_macbeth[18, 2] / response_macbeth[18, 1]
+    L1 = Lab1[:, 0]
+    A1 = Lab1[:,1]
+    B1 = Lab1[:, 2]
     
-    gains = [r_Gain, 1, b_Gain]
+    L2 = Lab2[:, 0]
+    A2 = Lab2[:,1]
+    B2 = Lab2[:, 2]
     
-    response_sensor = responses / gains
-    return response_sensor
+    kL = 1
+    kC = 1
+    kH = 1
+    
+    mask_value_0_input1=((A1==0)*(B1==0)).float()
+    mask_value_0_input2=((A2==0)*(B2==0)).float()
+    mask_value_0_input1_no=1-mask_value_0_input1
+    mask_value_0_input2_no=1-mask_value_0_input2
+    B1=B1+0.0001*mask_value_0_input1
+    B2=B2+0.0001*mask_value_0_input2 
+    
+    C1 = torch.sqrt((A1 ** 2.) + (B1 ** 2.))
+    C2 = torch.sqrt((A2 ** 2.) + (B2 ** 2.))   
+   
+    aC1C2 = (C1 + C2) / 2.
+    G = 0.5 * (1. - torch.sqrt((aC1C2 ** 7.) / ((aC1C2 ** 7.) + (25 ** 7.))))
+    a1P = (1. + G) * A1
+    a2P = (1. + G) * A2
+    c1P = torch.sqrt((a1P ** 2.) + (B1 ** 2.))
+    c2P = torch.sqrt((a2P ** 2.) + (B2 ** 2.))
+
+
+    h1P = hpf_diff(B1, a1P)
+    h2P = hpf_diff(B2, a2P)
+    h1P=h1P*mask_value_0_input1_no
+    h2P=h2P*mask_value_0_input2_no 
+    
+    dLP = L2 - L1
+    dCP = c2P - c1P
+    dhP = dhpf_diff(C1, C2, h1P, h2P)
+    dHP = 2. * torch.sqrt(c1P * c2P) * torch.sin(radians(dhP) / 2.)
+    mask_0_no=1-torch.max(mask_value_0_input1,mask_value_0_input2)
+    dHP=dHP*mask_0_no
+
+    aL = (L1 + L2) / 2.
+    aCP = (c1P + c2P) / 2.
+    aHP = ahpf_diff(C1, C2, h1P, h2P)
+    T = 1. - 0.17 * torch.cos(radians(aHP - 39)) + 0.24 * torch.cos(radians(2. * aHP)) + 0.32 * torch.cos(radians(3. * aHP + 6.)) - 0.2 * torch.cos(radians(4. * aHP - 63.))
+    dRO = 30. * torch.exp(-1. * (((aHP - 275.) / 25.) ** 2.))
+    rC = torch.sqrt((aCP ** 7.) / ((aCP ** 7.) + (25. ** 7.)))    
+    sL = 1. + ((0.015 * ((aL - 50.) ** 2.)) / torch.sqrt(20. + ((aL - 50.) ** 2.)))
+    
+    sC = 1. + 0.045 * aCP
+    sH = 1. + 0.015 * aCP * T
+    rT = -2. * rC * torch.sin(radians(2. * dRO))
+
+#     res_square=((dLP / (sL * kL)) ** 2.) + ((dCP / (sC * kC)) ** 2.) + ((dHP / (sH * kH)) ** 2.) + rT * (dCP / (sC * kC)) * (dHP / (sH * kH))
+
+    res_square=((dLP / (sL * kL)) ** 2.) + ((dCP / (sC * kC)) ** 2.)*mask_0_no + ((dHP / (sH * kH)) ** 2.)*mask_0_no + rT * (dCP / (sC * kC)) * (dHP / (sH * kH))*mask_0_no
+    mask_0=(res_square<=0).float()
+    mask_0_no=1-mask_0
+    res_square=res_square+0.0001*mask_0    
+    res=torch.sqrt(res_square)
+    res=res*mask_0_no
+    return torch.mean(res)
 
 def deltae_stats_nm(coef, X, y, degree, RP, n_terms):
 
--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
-from colour import MultiSpectralDistributions, read_sds_from_csv_file, MSDS_CAMERA_SENSITIVITIES, SpectralShape
+import torch
+
+from colour import MultiSpectralDistributions, read_sds_from_csv_file, MSDS_CAMERA_SENSITIVITIES, SpectralShape, read_spectral_data_from_csv_file
 from colour.colorimetry import sds_and_msds_to_msds, MSDS_CMFS_STANDARD_OBSERVER, SDS_ILLUMINANTS
 from colour.characterisation import normalise_illuminant
 
@@ -9,6 +11,38 @@ SIGMA = 'Sigma SDMerill (NPL)'
 CANON_PATH = "CANON.csv"
 ILLUMINANT = "D65"
 CMF_RANGE = SpectralShape(400, 700, 10)
+INTERP_CMF_RANGE = SpectralShape(400,700,10)
+
+def load_insitu(csv_file_path):
+    """
+    Load spectral data from a CSV file.
+    The first column is assumed to be non-spectral (e.g., an identifier).
+    The rest of the columns are assumed to be normalized reflectances with sampling 400:10:700 nm.
+    """
+    df = pd.read_csv(csv_file_path)
+    wavelengths  = df.iloc[:, 0].to_numpy()
+    values  = df.iloc[:, 1:].to_numpy()
+    return MultiSpectralDistributions(values, wavelengths).align(INTERP_CMF_RANGE)
+
+
+def msds_to_xyz(reflectances, sensitivities, illuminant):
+    response = (sensitivities.values.T @ (np.diag(illuminant.values) @ reflectances.values)).T
+    max_response = compute_response_perfect_reflector(sensitivities)
+    print(max_response / max_response[1])
+    return response / max_response[1]
+
+def msds_to_rgb(reflectances, sensitivities, illuminant):
+    response = (sensitivities.values.T @ (np.diag(illuminant.values) @ reflectances.values)).T
+    max_response = compute_response_perfect_reflector(sensitivities)
+    return response / max_response  
+
+
+def compute_response_perfect_reflector(sensitivities):
+    illuminant = SDS_ILLUMINANTS["D65"].align(INTERP_CMF_RANGE)
+    response_test = (sensitivities.values.T @ np.diag(illuminant.values)).T
+    response_sum = np.sum(response_test, axis=0)
+    
+    return response_sum
 
 
 def load_dataset_csv(csv_file_path):
@@ -19,7 +53,17 @@ def load_dataset_csv(csv_file_path):
     """
     df = pd.read_csv(csv_file_path)
     spectral_data = df.iloc[:, 1:].to_numpy()
-    return MultiSpectralDistributions(spectral_data.T, CMF_RANGE)
+    return MultiSpectralDistributions(spectral_data.T, CMF_RANGE).align(INTERP_CMF_RANGE)
+
+
+def load_dataset_skin():
+    """
+    Load spectral data from a CSV file.
+    The first column is assumed to be non-spectral (e.g., an identifier).
+    The rest of the columns are assumed to be normalized reflectances with sampling 400:10:700 nm.
+    """
+    skindata = read_sds_from_csv_file("Skin.csv")
+    return MultiSpectralDistributions(skindata).align(INTERP_CMF_RANGE)
 
 
 def load_dataset_sfu(file_path):
@@ -48,30 +92,27 @@ def load_dataset_sfu(file_path):
     
     data = {wavelength: reflectance_spectra[:, i] for i, wavelength in enumerate(wavelengths)}
     
-    return MultiSpectralDistributions(data).align(CMF_RANGE)
+    return MultiSpectralDistributions(data).align(INTERP_CMF_RANGE)
 
 
 def load_msds_nikon():
-    ssf = MSDS_CAMERA_SENSITIVITIES[NIKON].copy().align(CMF_RANGE)
-    illuminant = SDS_ILLUMINANTS[ILLUMINANT].align(CMF_RANGE)
-    return ssf, normalise_illuminant(illuminant, ssf)
+    ssf = MSDS_CAMERA_SENSITIVITIES[NIKON].copy().align(INTERP_CMF_RANGE)
+    return ssf
 
 def load_msds_sigma():
-    ssf = MSDS_CAMERA_SENSITIVITIES[SIGMA].copy().align(CMF_RANGE)
-    illuminant = SDS_ILLUMINANTS[ILLUMINANT].align(CMF_RANGE)
-    return ssf, normalise_illuminant(illuminant, ssf)
+    ssf = MSDS_CAMERA_SENSITIVITIES[SIGMA].align(INTERP_CMF_RANGE)
+    return ssf
     
 def load_msds_canon():
     ssf = sds_and_msds_to_msds(
         read_sds_from_csv_file(CANON_PATH).values()
-    ).copy().align(CMF_RANGE).normalise()
-    illuminant = SDS_ILLUMINANTS[ILLUMINANT].align(CMF_RANGE)
-    return ssf, normalise_illuminant(illuminant, ssf)
+    ).copy().align(INTERP_CMF_RANGE)
+    return ssf
     
-def load_illuminant_cmfs():
-    cmfs = MSDS_CMFS_STANDARD_OBSERVER['CIE 1931 2 Degree Standard Observer'].copy().align(CMF_RANGE)
-    illuminant = SDS_ILLUMINANTS[ILLUMINANT].align(CMF_RANGE)
-    return illuminant, cmfs
+def load_cmfs():
+    cmfs = MSDS_CMFS_STANDARD_OBSERVER['CIE 1931 2 Degree Standard Observer'].copy().align(INTERP_CMF_RANGE)
+    illuminant = SDS_ILLUMINANTS[ILLUMINANT].align(INTERP_CMF_RANGE)
+    return cmfs, illuminant
     
 def load_camera(camera):
     if camera == 'nikon':
@@ -82,3 +123,4 @@ def load_camera(camera):
         return load_msds_sigma()
     else:
         print("Invalid camera option, valid options are: nikon, canon, sigma")
+        
